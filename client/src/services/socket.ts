d40a4d5fab640dfd3ac14df7bc518813b678d5b2
xhuyz <xhuyzdev@gmail.com>
2025-09-30 23:31:02 +0700

    update

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from "socket.io-client";
 
-const URL = process.env.REACT_APP_SERVER_URL || "https://mln.xhuyz.me:4000";
+const URL = process.env.REACT_APP_SERVER_URL || "http://157.66.101.42:4000";
 
 export const socket: Socket = io(URL, {
-  autoConnect: false,
+  autoConnect: true,
 });
