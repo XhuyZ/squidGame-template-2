9e216f036ae565b8e8ebe176a805b5a4bc37b43c
xhuyz <xhuyzdev@gmail.com>
2025-09-30 23:46:16 +0700

    update

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -3,5 +3,5 @@ import { io, Socket } from "socket.io-client";
 const URL = process.env.REACT_APP_SERVER_URL || "http://157.66.101.42:4000";
 
 export const socket: Socket = io(URL, {
-  autoConnect: true,
+  autoConnect: false,
 });
